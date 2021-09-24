5b706aa8c3743afb8367557fc5b9b33bece58bc7
Mayara Martins <martinsmayara91@gmail.com>
2021-09-24 15:01:27 -0300

    ex

---
+++ ex9/ex9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+
+            if (N % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
+
